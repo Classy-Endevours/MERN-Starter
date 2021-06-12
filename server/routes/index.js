987797d0f2d5cfb03a6ddff90521f85fcba929fc
piyush dubey <piyush115dubey@gmail.com>
2021-06-12 23:07:53 +0530

    added some changes for workflow checker

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,7 +7,7 @@ router.get('/', function(req, res, next) {
   // const err = new Error('This is root route!') 
   // err.status = 404;  
   // next(err, req, res, next)
-  res.status(200).send('Welcome to the MERN starter backend!');
+  res.status(200).send('Welcome to the MERN starter backend, currently you are viewing starter kit!');
   // res.status(200).send({ title: 'Express' });
 }); 
 
